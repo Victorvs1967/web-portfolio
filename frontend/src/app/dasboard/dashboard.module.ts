07b358b36c822f5017bb5c0b2a7807a5cda596b0
Victorvs1967 <victorsmirnov@me.com>
2023-03-21 21:41:39 +0200

    add change

--- frontend/src/app/dasboard/dashboard.module.ts
+++ frontend/src/app/dasboard/dashboard.module.ts
@@ -6,21 +6,6 @@ import { HeaderComponent } from './component/header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgChartsModule } from 'ng2-charts';
 import { DashboardroutingModule } from './dashboard-routing.module';
-<<<<<<< HEAD
-import { AddImageComponent } from './component/dashboard/component/add-image/add-image.component';
-import { AddProjectComponent } from './component/dashboard/component/add-project/add-project.component';
-import { AddSkillComponent } from './component/dashboard/component/add-skill/add-skill.component';
-import { EditProjectComponent } from './component/dashboard/component/edit-project/edit-project.component';
-import { EditSkillComponent } from './component/dashboard/component/edit-skill/edit-skill.component';
-import { EditUserComponent } from './component/dashboard/component/edit-user/edit-user.component';
-import { GraphComponent } from './component/dashboard/component/graph/graph.component';
-import { ListImageComponent } from './component/dashboard/component/list-image/list-image.component';
-import { ProjectListComponent } from './component/dashboard/component/list-image/project-list/project-list.component';
-import { ListProjectComponent } from './component/dashboard/component/list-project/list-project.component';
-import { ListSkillComponent } from './component/dashboard/component/list-skill/list-skill.component';
-import { ListUserComponent } from './component/dashboard/component/list-user/list-user.component';
-import { ViewImageComponent } from './component/dashboard/component/view-image/view-image.component';
-=======
 import { AddImageComponent } from './component/add-image/add-image.component';
 import { AddProjectComponent } from './component/add-project/add-project.component';
 import { AddSkillComponent } from './component/add-skill/add-skill.component';
@@ -34,7 +19,6 @@ import { ListUserComponent } from './component/list-user/list-user.component';
 import { ViewImageComponent } from './component/view-image/view-image.component';
 // import { ListProjectComponent } from './component/list-project/list-project.component';
 // import { ListSkillComponent } from './component/list-skill/list-skill.component';
->>>>>>> 33a2497 (correct directory structure)
 
 @NgModule({
   declarations: [
